cae5d0b4626325f15461ad0804ba528d981c8054
AlexandreDev <alexandreagradev@gmail.com>
2022-12-09 12:49:05 -0300

    Initial commit

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "bc212c7fd1b1d2c5bf84e70a32cfd6e2"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
